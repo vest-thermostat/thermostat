fae907add4a10bb366ac23db166b682c36027e63
Perale Thomas <thomas.perale@openmailbox.org>
2017-01-22 01:49:52 +0100

    Writting a DHT monitoring automaton based on Atm_timer

---
+++ src/Atm_DHT.cpp
@@ -0,0 +1,95 @@
+#include "Atm_DHT.h"
+
+/* Add optional parameters for the state machine to begin()
+ * Add extra initialization code
+ */
+
+Atm_DHT& Atm_DHT::begin(uint8_t pin, uint8_t type, uint16_t delay) {
+  // clang-format off
+  const static state_t state_table[] PROGMEM = {
+    /*                   ON_ENTER  ON_LOOP  ON_EXIT  EVT_INCORRECT_DATAS  EVT_CORRECT_DATAS  EVT_TIMEOUT  EVT_START  ELSE */
+    /*      IDLE */            -1,      -1,      -1,                  -1,                -1,          -1,   WAITING,   -1,
+    /*   WAITING */   ENT_WAITING,      -1,      -1,                  -1,                -1,      FINISH,        -1,   -1,
+    /*    FINISH */    ENT_FINISH,      -1,      -1,             WAITING,         NEW_DATAS,          -1,        -1,   -1,
+    /* NEW_DATAS */ ENT_NEW_DATAS,      -1,      -1,                  -1,                -1,          -1,        -1,   -1,
+  };
+  // clang-format on
+  Machine::begin( state_table, ELSE );
+
+  _pin = pin;
+  _type = type;
+  _delay = delay;
+
+  return *this;
+}
+
+/* Add C++ code for each internally handled event (input)
+ * The code must return 1 to trigger the event
+ */
+
+int Atm_DHT::event( int id ) {
+  switch ( id ) {
+    case EVT_INCORRECT_DATAS:
+      return 0;
+    case EVT_CORRECT_DATAS:
+      return 0;
+    case EVT_TIMEOUT:
+      return mstimer.expired(this);
+    case EVT_START:
+      return 0;
+  }
+  return 0;
+}
+
+/* Add C++ code for each action
+ * This generates the 'output' for the state machine
+ */
+
+void Atm_DHT::action( int id ) {
+  switch ( id ) {
+    case ENT_WAITING:
+      mstimer.set(_delay);
+      Serial.print("Delay set to ");
+      Serial.println(_delay);
+      return;
+    case ENT_FINISH:
+      Serial.println("FINISH");
+      return;
+    case ENT_NEW_DATAS:
+      return;
+  }
+}
+
+/* Public event methods
+ *
+ */
+
+Atm_DHT& Atm_DHT::incorrect_datas() {
+  trigger( EVT_INCORRECT_DATAS );
+  return *this;
+}
+
+Atm_DHT& Atm_DHT::correct_datas() {
+  trigger( EVT_CORRECT_DATAS );
+  return *this;
+}
+
+Atm_DHT& Atm_DHT::timeout() {
+  trigger( EVT_TIMEOUT );
+  return *this;
+}
+
+Atm_DHT& Atm_DHT::start() {
+  trigger( EVT_START );
+  return *this;
+}
+
+/* State trace method
+ * Sets the symbol table and the default logging method for serial monitoring
+ */
+
+Atm_DHT& Atm_DHT::trace( Stream & stream ) {
+  Machine::setTrace( &stream, atm_serial_debug::trace,
+    "DHT\0EVT_INCORRECT_DATAS\0EVT_CORRECT_DATAS\0EVT_TIMEOUT\0EVT_START\0ELSE\0IDLE\0WAITING\0FINISH\0NEW_DATAS" );
+  return *this;
+}
---
+++ src/Atm_DHT.h
@@ -0,0 +1,88 @@
+#ifndef ATM_DHT_H
+#define ATM_DHT_H
+
+#if ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include <Automaton.h>
+#include <string>
+#include <string.h>
+
+#ifdef DHT_DEBUG
+  #define DEBUG_PRINT(...) { Serial.print(__VA_ARGS__); }
+  #define DEBUG_PRINTLN(...) { Serial.println(__VA_ARGS__); }
+#else
+  #define DEBUG_PRINT(...) {}
+  #define DEBUG_PRINTLN(...) {}
+#endif
+
+// Define types of sensors.
+#define DHT11 11
+#define DHT22 22
+#define DHT21 21
+#define AM2301 21
+
+typedef struct {
+  float temperature;
+  float humidity;
+} SensorDatas;
+
+typedef void (*DHTCallback)(SensorDatas);
+
+class Atm_DHT: public Machine {
+  private:
+    class InterruptLock {
+      public:
+        InterruptLock() {
+          noInterrupts();
+        }
+
+        ~InterruptLock() {
+          interrupts();
+        }
+    };
+
+    // BASIC DATA FOR CONFIGURATION
+    uint8_t _pin;
+    uint8_t _type;
+    uint16_t _delay;
+
+    // AUTOMATON DATAS
+    enum { ENT_WAITING, ENT_FINISH, ENT_NEW_DATAS }; // ACTIONS
+    int event( int id );
+    void action( int id );
+    atm_timer_millis mstimer;
+    DHTCallback _callback;
+
+    // DATA FROM THE SENSOR
+    uint8_t _rawdata[5];
+    uint32_t _maxcycles;
+    SensorDatas _datas;
+
+    uint32_t expectPulse(bool);
+    bool _read ();
+    float getTemperature();
+    float getHumidity();
+
+  public:
+    enum { IDLE, WAITING, FINISH, NEW_DATAS }; // STATES
+    enum { EVT_INCORRECT_DATAS, EVT_CORRECT_DATAS, EVT_TIMEOUT, EVT_START, ELSE }; // EVENTS
+    Atm_DHT(void): Machine() {
+      _maxcycles = microsecondsToClockCycles(1000);
+      // 1 millisecond timeout for
+      // reading pulses from DHT sensor.
+      // Note that count is now ignored as the DHT reading algorithm adjusts itself
+      // basd on the speed of the processor.
+    };
+    Atm_DHT& begin(uint8_t pin, uint8_t type, uint16_t delay);
+    Atm_DHT& trace( Stream & stream );
+    Atm_DHT& incorrect_datas( void );
+    Atm_DHT& correct_datas( void );
+    Atm_DHT& timeout( void );
+    Atm_DHT& start( void );
+    Atm_DHT& onNewDatas(DHTCallback);
+};
+#endif
